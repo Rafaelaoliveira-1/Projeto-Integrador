2443339b9444e0df6a00c1240b13d8b8e96f0384
Rafaela Oliveira <rafaela.oliveira_@hotmail.com>
2021-01-29 12:33:38 -0300

    CRUD Tema e Postagem

--- ADA/src/main/java/com/ada/ADA/model/Tema.java
+++ ADA/src/main/java/com/ada/ADA/model/Tema.java
@@ -8,73 +8,46 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
-	@Entity
-	@Table(name = "tb_tema")
-	public class Tema {
-
-	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY) //É usado para aviso para o JPA para realizar a conversão de uma classe. 
-	private long id_tema;
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY) //É usado para avisar o JPA realizar a conversão de uma classe. 
+	private long id_tema;
+		
 	@NotNull
 	@Size(min = 2 , max = 50)
 	private String descricaoTema; //Tema usado para descrever a postagem e pesquisa. Terá no máximo 50 caracteres. 
-	
-	@NotNull
-	@Size(min = 2 , max = 50)
-	private String descricaoDuvida; //Usado para pesquisar no fórum pesquisa de tópicos. Terá no máximo 50 caracteres. 
-	
-	@NotNull
-	private boolean vaga; // Usado para pesquisar vagas de emprego na Rede Social. 
-	
-	@NotNull
-	private boolean duvida; //Usado para checar se a usuária deseja postar no fórum pesquisa de tópicos.
 		
-	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String tipoTema; // vaga, duvida
+
 //Início Getters and Setters
 	
-	public long getId_tema() {
-		return id_tema;
-	}
+public long getId_tema() {
+	return id_tema;
+}
 
-	public void setId_tema(long id_tema) {
-		this.id_tema = id_tema;
-	}
+public void setId_tema(long id_tema) {
+	this.id_tema = id_tema;
+}
 		
-	public String getDescricaoTema() {
-		return descricaoTema;
-	}
+public String getDescricaoTema() {
+	return descricaoTema;
+}
 
-	public void setDescricaoTema(String descricaoTema) {
-		this.descricaoTema = descricaoTema;
-	}
-	
-	public boolean isVaga() {
-		return vaga;
-	}
-	
-	public void setVaga(boolean vaga) {
-		this.vaga = vaga;
-	}
-	
-	public String getDescricaoDuvida() {
-		return descricaoDuvida;
-	}
-	
-	public void setDescricaoDuvida(String descricaoDuvida) {
-		this.descricaoDuvida = descricaoDuvida;
-	}
-	
-	public boolean isDuvida() {
-		return duvida;
-	}
-	
-	public void setDuvida(boolean duvida) {
-		this.duvida = duvida;
-	}
-	
-//Fim Getters and Setters
-	
-	
+public void setDescricaoTema(String descricaoTema) {
+	this.descricaoTema = descricaoTema;
+}
+
+public String getTipoTema() {
+	return tipoTema;
+}
+
+public void setTipoTema(String tipoTema) {
+	this.tipoTema = tipoTema;
+}
+//Fim Getters and Setters	
 }
